822557c4baaae5c7673de96dca741bf43bf98ed1
Jwilss3 <jessicalaurenwilson1@gmail.com>
2022-03-07 12:20:11 +0000

    Add components directory. Create header component and render it in the app. Amend CSS for header.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Header } from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header className="app-header">
+        <Header />
       </header>
     </div>
   );
