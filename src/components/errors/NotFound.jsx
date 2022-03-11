e8f7c7ba045633cbd09e7dc4efcbab0aaa4f1041
Jwilss3 <jessicalaurenwilson1@gmail.com>
2022-03-11 08:37:21 +0000

    Style NotFound component for 404 error

--- src/components/errors/NotFound.jsx
+++ src/components/errors/NotFound.jsx
@@ -2,9 +2,11 @@ import { Link } from 'react-router-dom';
 
 export default function NotFound() {
     return (
-        <>
-            <p>Oops! Status: 404 - Not Found</p>
-            <Link to='/'>Let's go home</Link>
-        </>
+        <div className='error-nf'>
+            <div className='not-found'>
+                <p>Oops! Status: 404 - Not Found</p>
+                <Link to='/'>Let's go home</Link>
+            </div>
+        </div>
     )
 }
