796f59efd91472917b2e22ed29f9faa73352a2d2
Jwilss3 <jessicalaurenwilson1@gmail.com>
2022-04-11 14:40:59 +0100

    Allow a user to delete their own comments

--- src/components/articles/CommentsCard.jsx
+++ src/components/articles/CommentsCard.jsx
@@ -1,36 +1,60 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { fetchCommentsById } from '../../utils/api'
-import PostComment from './PostComment'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCommentsById, deleteCommentById } from '../../utils/api';
+import PostComment from './PostComment';
 
 export default function CommentsCard() {
-    let { article_id } = useParams()
-    const [comment, setComment] = useState()
-    const [isLoading, setIsLoading] = useState(true)
+    let { article_id } = useParams();
+    const [comment, setComment] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+    const [deleteSuccess, setDeleteSuccess] = useState(false);
 
     useEffect(() => {
-        setIsLoading(true)
-        fetchCommentsById(article_id).then((comment) => {
-            setComment(comment)
-            setIsLoading(false)
-        }).catch(() => {
-        })
-    }, [article_id])
+        setIsLoading(true);
+        fetchCommentsById(article_id)
+            .then((comment) => {
+                setComment(comment);
+                setIsLoading(false);
+            })
+            .catch(() => { });
+    }, [article_id]);
+
+    const handleDelete = (e) => {
+        e.preventDefault();
+        const id = e.target.value;
+        setComment(() => {
+            const updatedComments = comment.filter((comment) => {
+                return comment.comment_id !== parseInt(id);
+            });
+            return updatedComments;
+        });
+        deleteCommentById(id);
+        setDeleteSuccess(true);
+    };
 
     if (isLoading) return <p>loading..</p>;
-    return (<>
-        <PostComment />
-        {
-            comment.map((comments) => {
+    return (
+        <>
+            <PostComment />
+            {deleteSuccess && <p>Comment deleted!</p>}
+            {comment.map((comments) => {
                 return (
                     <section key={comments.comment_id} className='comments'>
                         <h2 className='comment-page-author'>{comments.author}</h2>
                         <p className='comment-page-body'>{comments.body}</p>
+                        {comments.author === 'cooljmessy' && (
+                            <button
+                                className='delete-comment'
+                                value={comments.comment_id}
+                                onClick={handleDelete}
+                            >
+                                Delete
+                            </button>
+                        )}
                         <p className='comment-page-votes'>votes: {comments.votes}</p>
                     </section>
-                )
-            })
-        }
-    </>
-    )
+                );
+            })}
+        </>
+    );
 }
