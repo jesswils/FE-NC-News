3d93295decb4f0c8f018b93123b66ca062a222e1
Jwilss3 <jessicalaurenwilson1@gmail.com>
2022-04-13 18:14:40 +0100

    Update file and component name changes in app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import { Routes, Route, } from 'react-router-dom'
 import { ArticleCard } from './components/ArticleCard';
 import { Header } from './components/Header';
-import NavBar from './components/NavBar';
-import { TopicsCard } from './components/TopicsPage'
-import ArticlesPage from './components/ArticlesPage';
+import { NavBar } from './components/NavBar';
+import { Topics } from './components/Topics'
+import Articles from './components/Articles';
 import NotFound from './components/errors/NotFound'
 import CommentsCard from './components/CommentsCard';
+import Welcome from './components/Welcome';
 
 function App() {
 
@@ -17,11 +18,11 @@ function App() {
       </header>
       <NavBar />
       <Routes>
-        <Route path='/' element={<ArticleCard />} />
-        <Route path="/topics" element={<TopicsCard />} />
+        <Route path='/' element={<Welcome />} />
+        <Route path="/topics" element={<Topics />} />
         <Route path="/articles" element={<ArticleCard />} />
         <Route path='/topics/:topic' element={<ArticleCard />} />
-        <Route path='/articles/:article_id' element={<ArticlesPage />} />
+        <Route path='/articles/:article_id' element={<Articles />} />
         <Route path='/articles/:article_id/comments' element={<CommentsCard />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
