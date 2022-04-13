2754df3c06226de35ba356bdbede23449fb5bbfa
Jwilss3 <jessicalaurenwilson1@gmail.com>
2022-04-13 18:17:11 +0100

    Create homepage component

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/logo.png'
+
+export default function Welcome() {
+    return (
+        <div>
+            <h2>Welcome to NC News</h2>
+            <img src={logo} alt={"logo"} className='logo' /></div>
+
+    )
+}
