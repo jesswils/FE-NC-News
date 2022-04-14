ceb3282e29b24b9e9d621a4c9a9e90b7071b6705
Jwilss3 <jessicalaurenwilson1@gmail.com>
2022-04-14 11:53:24 +0100

    Add image assets for topics page

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,19 +1,25 @@
 import { Link } from 'react-router-dom'
+import coding from '../assets/coding.png'
+import cooking from '../assets/cooking.png'
+import football from '../assets/football.png'
 
 export const Topics = () => {
     return (
         <div><h1 className='topics-header'>Topics:</h1>
             <div className='topic-links-outer'>
                 <div className='topic-links-inner'>
-                    <Link to="/topics/coding" className='coding-link'>Coding</Link>
+                    <Link to="/topics/coding" className='topic-links'>Coding</Link>
                     <br></br>
                     <br></br>
+                    <img src={coding} alt={"coding-icon"} className='topic-icon' />
+                    <Link to="/topics/cooking" className='topic-links'>Cooking</Link>
                     <br></br>
-                    <Link to="/topics/cooking" className='cooking-link'>Cooking</Link>
+                    <br></br>
+                    <img src={cooking} alt={"cooking-icon"} className='topic-icon' />
+                    <Link to="/topics/football" className='topic-links'>Football</Link>
                     <br></br>
                     <br></br>
-                    <br></br>
-                    <Link to="/topics/football" className='football-link'>Football</Link>
+                    <img src={football} alt={"cooking-icon"} className='topic-icon' />
                 </div>
             </div>
         </div>
