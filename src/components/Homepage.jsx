b3428a978a7f5b56c915715e6217aefd6039f9a5
Jwilss3 <jessicalaurenwilson1@gmail.com>
2022-04-15 14:42:45 +0100

    Rename welcome to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,15 +6,14 @@ export default function Welcome() {
         <div>
             <h1 className='homepage-header'>Welcome!</h1>
             <h2 className='homepage-tagline'> NC News is a social news website</h2>
+            <p className='homepage-paragraph'>Look through a selection of articles or browse via topics</p>
             <div className='homepage'>
                 <img src={logo} alt={"logo"} className='logo' />
                 <img src={logo} alt={"logo"} className='logo' />
-
-                <p className='homepage-paragraph'>Look through a selection of articles or browse via topics</p>
-                <div className='button-wrapper'>
-                    <Link to="/articles" className='homepage-button'>Articles</Link >
-                    <Link to="/topics" className='homepage-button'>Topics</Link>
-                </div>
+            </div>
+            <div className='button-wrapper'>
+                <Link to="/articles" className='homepage-button'>Articles</Link >
+                <Link to="/topics" className='homepage-button'>Topics</Link>
             </div>
         </div>
     )
